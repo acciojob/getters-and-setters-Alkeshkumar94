69b27f80d2ee92eea7e552ed4c6efdfec379817b
Alkesh kumar <127075190+Alkeshkumar94@users.noreply.github.com>
2024-04-25 09:38:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+		this.name=name;
+		this.age=age;
+	}
+	get _name(){
+		return this.name;
+	}
+	set _age(){
+		return this.age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	super(name,age);
+	study(){
+		console.log(`${name} is studying`);
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	super(name,age);
+	teach(){
+		console.log(`${name} is teaching`);
+	}
+}
 
+const person = new Person("John", 25);
+console.log(person.name);
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
