e8ba353b58e17f0dd8824559f416093ae54bca1d
Alkesh kumar <127075190+Alkeshkumar94@users.noreply.github.com>
2024-05-13 09:39:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,47 +1,26 @@
-function Person(name, age) {
-  this._name = name;
-  this._age = age;
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set age(age) {
+    this._age = age;
+  }
 }
 
-Person.prototype.getName = function() {
-  return this._name;
+class Student extends Person {
+  study() {
+    console.log(`${this.name} is studying`);
+  }
 }
 
-Person.prototype.setAge = function(age) {
-  this._age = age;
+class Teacher extends Person {
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
 }
-
-Person.prototype.getAge = function() {
-  return this._age;
-}
-
-function Student(name, age) {
-  Person.call(this, name, age);
-}
-
-Student.prototype = Object.create(Person.prototype);
-Student.prototype.study = function() {
-  console.log(this.getName() + ' is studying');
-}
-
-function Teacher(name, age) {
-  Person.call(this, name, age);
-}
-
-Teacher.prototype = Object.create(Person.prototype);
-Teacher.prototype.teach = function() {
-  console.log(this.getName() + ' is teaching');
-}
-
-// Example usage:
-var person = new Person("John", 25);
-console.log(person.getName());
-
-person.setAge(30);
-console.log(person.getAge());
-
-var student = new Student("Alice", 22);
-student.study();
-
-var teacher = new Teacher("Bob", 40);
-teacher.teach();
