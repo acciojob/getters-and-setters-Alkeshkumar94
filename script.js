fdb41fbb14fab3126c856de3ec4e167acb401315
Alkesh kumar <127075190+Alkeshkumar94@users.noreply.github.com>
2024-06-03 08:07:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,12 +16,19 @@ get age(){
 }
 
 class Student extends Person {
+	 constructor(name, age) {
+        super(name, age);
+    }
   study() {
     console.log(`${this.name} is studying`);
   }
 }
 
 class Teacher extends Person {
+	 constructor(name, age) {
+        super(name, age);
+    }
+
   teach() {
     console.log(`${this.name} is teaching`);
   }
